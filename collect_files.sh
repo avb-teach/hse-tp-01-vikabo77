f5d172b8a9cea324fe52845100a348b3840a3c89
boooo <viiiiiika>
2025-05-02 10:10:15 +0300

    uuuu

--- collect_files.sh
+++ collect_files.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+input_dir="$1"
+output_dir="$2"
 new_name() {
-    local i=1
-    local name="${1%.*}"
-    local ext="${1##*.}"
-    local new="$1"
-    while [ -e "$2/$new" ]; do
-        new="${name}_$((i++)).${ext}"
+    local original_name="$1"
+    local name="${original_name%.*}"
+    local ext="${original_name##*.}"
+    local n_name="$original_name"
+    local count=1
+    while [ -e "$output_dir/$n_name" ]; do
+        n_name="${name}_${count}.${ext}"
+        ((count++))
     done
-    echo "$new"
+    echo "$new_name"
 }
-input="$1"
-output="$2"
-find "$input" -type f -print0 | while IFS= read -r -d '' f; do
-    cp "$f" "$output/$(new_name "${f##*/}" "$output)"
+find "$input_dir" -type f -print0 | while IFS= read -r -d '' file; do
+    file_name=$(basename "$file")
+    unique_name=$(new_name "$file_name")
+    cp "$file" "$output_dir/$unique_name"
 done
